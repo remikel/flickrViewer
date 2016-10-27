fff4ec1de909722f0738a98bab1ed5e2e767b276
idsdev@Mac-mini-de-Remi.local <remi.mikel@habiteo.com>
2016-10-27 13:27:07 +0200

    history

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,34 +1,47 @@
 var app = angular.module('starter', ['ionic']);
 
-app.controller('myCtrl', function($scope, $http, $location) {
-	var page = 1;
-	var tag = 'inspection';
-	window.localStorage["key"] = angular.toJson(new Array());
-	$scope.images = [];
+app.controller('myCtrl', function($scope, $http, $location, $window) {
+	var paramValue;
+	var page;
+	var tag;
+	var refresh = function(){
+		paramValue = $location.path();
+		page = 1;
+		tag = $location.path() ? $location.path().slice(1) : 'inspection';
+		window.localStorage["key"] = angular.toJson(new Array());
+		$scope.images = [];
+		$scope.tag = tag;
+		$scope.showMore();
+	}
 	$scope.showMore = function() {
 		$http.get('https://api.flickr.com/services/rest/?api_key=f75bf400c7720eb42bd8b8dd14765dfc&method=flickr.photos.search&format=json&nojsoncallback=?&tags=' + tag + '&per_page=3&page=' + page)
 		.success(function(data) {
 			$scope.images = $scope.images.concat(data['photos']['photo']);
 			$scope.$broadcast('scroll.infiniteScrollComplete');
-			console.log(data['photos']['photo'].length);
 		}).error(function(data){
-			console.log(data);
 		});
 		page++;
-	};	
+	};
+	refresh();	
 	$scope.search = function(){
 		if ($scope.tag == '' || $scope.tag == tag)
 			return;
-		page = 1;
 		tag = $scope.tag;
 		// historic = angular.fromJson(window.localStorage["key"]);
 		// historic = historic.concat(tag);
-		// $location.path(tag);
 		// window.localStorage["key"] = angular.toJson(historic);
-		$http.get('https://api.flickr.com/services/rest/?api_key=f75bf400c7720eb42bd8b8dd14765dfc&method=flickr.photos.search&format=json&nojsoncallback=?&tags=' + tag + '&per_page=3&page=' + page).success(function(data) {
-			$scope.images = data['photos']['photo'];
-		}).error(function(data){
-			console.log(data);
-		});
+		$location.path(tag);
+		refresh();
 	}
+	$scope.$on('$pathChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+
+	console.log("State changed: ", toState);
+
+		$scope.refreshItems();
+	});
+	$scope.$on('$locationChangeStart', function(event) {
+		console.log("State changed: ");
+		refresh();
+	    // $scope.refreshItems();
+	}); 
 });
