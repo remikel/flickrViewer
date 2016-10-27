263cad58600e8d5243852a2594d5140f13853b9e
Remi Mikel <remikel0729@gmail.com>
2016-10-27 23:41:12 +0200

    save search, beautify and comments

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,31 +1,20 @@
 var app = angular.module('starter', ['ionic']);
 
 app.controller('myCtrl', function($scope, $http, $location, $window) {
-	    // mock des cartes dans lesquelles rechercher
-	// $scope.tags = [
-	// 	{ 'name' : 'Skylasher' },
-	// 	{ 'name' : 'Thrashing Mossdog' },
-	// 	{ 'name' : 'Zhur-Taa Druid' },
-	// 	{ 'name' : 'Feral Animist' },
-	// 	{ 'name' : 'Rubblebelt Maaka' },
-	// 	{ 'name' : 'Mending Touch' },
-	// 	{ 'name' : 'Weapon Surge' },
-	// 	{ 'name' : 'Woodlot Crawler' },
-	// 	{ 'name' : 'Phytoburst' },
-	// 	{ 'name' : 'Smelt-Ward Gatekeepers' },
-	// 	{ 'name' : 'Debt to the Deathless' },
-	// 	{ 'name' : 'Woodlot Crawler' },
-	// 	{ 'name' : 'Blaze Commando' },
-	// 	{ 'name' : 'Uncovered Clues' }
-	// ];
- 
-	// saisie du nom de la carte
-	$scope.tags = [];
-
 	var paramValue;
 	var page;
 	var tag;
-		window.localStorage["key"] = angular.toJson(new Array());
+	var params = {
+		api_key : 'f75bf400c7720eb42bd8b8dd14765dfc',
+		method : 'flickr.photos.search',
+		format : 'json',
+		nojsoncallback : 'true',
+		per_page : 3,
+	}
+	// get previous research from localstorage for prediction
+	$scope.tags = typeof window.localStorage["tags"] !== 'undefined' ? angular.fromJson(window.localStorage["tags"]) : [];
+
+	// To refresh and get value
 	var refresh = function(){
 		paramValue = $location.path();
 		page = 1;
@@ -35,23 +24,33 @@ app.controller('myCtrl', function($scope, $http, $location, $window) {
 		$scope.showMore();
 		$scope.moredata = false;
 	}
+
+	// Load pictures and the next
 	$scope.showMore = function() {
-		$http.get('https://api.flickr.com/services/rest/?api_key=f75bf400c7720eb42bd8b8dd14765dfc&method=flickr.photos.search&format=json&nojsoncallback=?&tags=' + tag + '&per_page=3&page=' + page)
+		params.tags = tag;
+		params.page = page;
+		$http.get('https://api.flickr.com/services/rest/', { params : params })
 		.success(function(data) {
 			if (data['photos']['photo'].length == 0)
 				$scope.moredata = true;
 			$scope.images = $scope.images.concat(data['photos']['photo']);
 			$scope.$broadcast('scroll.infiniteScrollComplete');
 		}).error(function(data){
+			$scope.moredata = true;
 		});
 		page++;
 	};
+
+	// Relocate and record research
 	$scope.search = function(){
 		if ($scope.tag == '' || $scope.tag == tag)
 			return;
 		tag = $scope.tag;
-		if ($scope.tags.indexOf(tag) == -1)
-			$scope.tags = $scope.tags.concat(tag);
+		if ($scope.tags && $scope.tags.indexOf(tag) === -1)
+		{
+			$scope.tags.push(tag);
+			window.localStorage["tags"] = angular.toJson($scope.tags);
+		}
 		$location.path(tag);
 	}
 	$scope.$on('$locationChangeStart', function(event) {
