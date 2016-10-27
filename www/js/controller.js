40f5a21f630c9c1be6dbbc93309043f20042350d
idsdev@Mac-mini-de-Remi.local <remi.mikel@habiteo.com>
2016-10-27 18:52:31 +0200

    search and fixes

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,47 +1,60 @@
 var app = angular.module('starter', ['ionic']);
 
 app.controller('myCtrl', function($scope, $http, $location, $window) {
+	    // mock des cartes dans lesquelles rechercher
+	// $scope.tags = [
+	// 	{ 'name' : 'Skylasher' },
+	// 	{ 'name' : 'Thrashing Mossdog' },
+	// 	{ 'name' : 'Zhur-Taa Druid' },
+	// 	{ 'name' : 'Feral Animist' },
+	// 	{ 'name' : 'Rubblebelt Maaka' },
+	// 	{ 'name' : 'Mending Touch' },
+	// 	{ 'name' : 'Weapon Surge' },
+	// 	{ 'name' : 'Woodlot Crawler' },
+	// 	{ 'name' : 'Phytoburst' },
+	// 	{ 'name' : 'Smelt-Ward Gatekeepers' },
+	// 	{ 'name' : 'Debt to the Deathless' },
+	// 	{ 'name' : 'Woodlot Crawler' },
+	// 	{ 'name' : 'Blaze Commando' },
+	// 	{ 'name' : 'Uncovered Clues' }
+	// ];
+ 
+	// saisie du nom de la carte
+	$scope.tags = [];
+
 	var paramValue;
 	var page;
 	var tag;
+		window.localStorage["key"] = angular.toJson(new Array());
 	var refresh = function(){
 		paramValue = $location.path();
 		page = 1;
 		tag = $location.path() ? $location.path().slice(1) : 'inspection';
-		window.localStorage["key"] = angular.toJson(new Array());
 		$scope.images = [];
 		$scope.tag = tag;
 		$scope.showMore();
+		$scope.moredata = false;
 	}
 	$scope.showMore = function() {
 		$http.get('https://api.flickr.com/services/rest/?api_key=f75bf400c7720eb42bd8b8dd14765dfc&method=flickr.photos.search&format=json&nojsoncallback=?&tags=' + tag + '&per_page=3&page=' + page)
 		.success(function(data) {
+			if (data['photos']['photo'].length == 0)
+				$scope.moredata = true;
 			$scope.images = $scope.images.concat(data['photos']['photo']);
 			$scope.$broadcast('scroll.infiniteScrollComplete');
 		}).error(function(data){
 		});
 		page++;
 	};
-	refresh();	
 	$scope.search = function(){
 		if ($scope.tag == '' || $scope.tag == tag)
 			return;
 		tag = $scope.tag;
-		// historic = angular.fromJson(window.localStorage["key"]);
-		// historic = historic.concat(tag);
-		// window.localStorage["key"] = angular.toJson(historic);
+		if ($scope.tags.indexOf(tag) == -1)
+			$scope.tags = $scope.tags.concat(tag);
 		$location.path(tag);
-		refresh();
 	}
-	$scope.$on('$pathChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
-
-	console.log("State changed: ", toState);
-
-		$scope.refreshItems();
-	});
 	$scope.$on('$locationChangeStart', function(event) {
-		console.log("State changed: ");
 		refresh();
-	    // $scope.refreshItems();
 	}); 
 });
